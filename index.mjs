af37287442d1069ebb1869fbed864461ba547e78
atoga22 <atogasto22@gmail.com>
2025-04-06 01:13:34 +0900

    機能完了

--- index.mjs
+++ index.mjs
@@ -4,6 +4,11 @@ const onClickAdd = () => {
     //テキストボックスの値を取得して初期化する
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value="";
+    createIncompleteTodo(inputText);
+}
+
+//渡された引数をもとに、未完了のTODOを作成する関数
+const createIncompleteTodo = (todo) => {
 
     //以下でリストの要素を生成する記述　li div p button
     const li = document.createElement("li");
@@ -12,7 +17,7 @@ const onClickAdd = () => {
     
     const p = document.createElement("p");
     p.className = "todo-item";
-    p.innerText = inputText;
+    p.innerText = todo;
 
     const completeButton = document.createElement("button");
     completeButton.innerText="完了";
@@ -24,9 +29,19 @@ const onClickAdd = () => {
         //戻すボタンを生成して押されたボタン親のli（moveTarget）配下に設定
         const backButton = document.createElement("button");
         backButton.innerText = "戻す";
+        backButton.addEventListener("click",()=>{
+            //　戻すボタンの機能実装　　➡︎　押されたボタンの前にあるテキストを取得して、そのテキストをもとに未完了リストに入れる関数（createIncompleteTodo）を実行
+            const todoText = backButton.previousElementSibling.innerText; //previous~で前の要素を取得　＝　リストのテキスト部分
+            createIncompleteTodo(todoText);
+            //戻すボタンとそのテキストを削除する必要あり
+            backButton.closest("li").remove(); //戻すボタンが押されたリストごと削除してしまう
+        })
+        
+        
         moveTarget.firstElementChild.appendChild(backButton); //戻すボタンを子要素追加　　first~で一番最初の子要素　　　つまりli（moveTarget）の最初の子要素　→divタグ
         //完了リストに移動 liタグまるごと移動
         document.getElementById("complete-list").appendChild(moveTarget);
+        
     });
 
     const deleteButton = document.createElement("button");
@@ -45,7 +60,8 @@ const onClickAdd = () => {
     
     //未完了リストへ追加　　incomplete-listは html側でulタグ　に指定　その子要素に上で作ったliを指定
     document.getElementById("incomplete-list").appendChild(li);
+};
 
-}
+
 
 document.getElementById("add-button").addEventListener("click",onClickAdd);
